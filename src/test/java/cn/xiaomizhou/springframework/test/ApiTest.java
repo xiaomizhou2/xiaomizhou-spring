2e5e8fc0bd3cf52733766ac0924f7b2deb73fcb7
xiaomizhou <937031323@qq.com>
2021-12-01 13:17:09 +0800

    update

--- src/test/java/cn/xiaomizhou/springframework/test/ApiTest.java
+++ src/test/java/cn/xiaomizhou/springframework/test/ApiTest.java
@@ -20,7 +20,7 @@ public class ApiTest {
         BeanDefinition userBeanDefinition = new BeanDefinition(new UserService());
         factory.registerBeanDefinition("userService", userBeanDefinition);
 
-        //3.获取bean123
+        //3.获取bean
         UserService userService = (UserService) factory.getBean("userService");
         userService.queryUserInfo();
     }
